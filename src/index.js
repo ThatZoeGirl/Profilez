aed9bee5cfa8d17bcde63e7e902ba3e4f1bedd32
Zoë <that.silly.zoe.girl@outlook.com>
2025-08-14 16:22:54 +0200

    Readied up for zCord and the new database

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@
 const dotenv = require('dotenv');
 dotenv.configDotenv();
 
+/* node */
+const path = require('node:path');
+
 /* Postgre */
-const profilesClient = require("../lib/profilesDB.js").profileDBPrismaClient;
-const serversClient = require("../lib/serversDB.js").serverDBPrismaClient;
+const rapaelClient = require("../lib/raphaelDB.js").rapaelDBPrismaClient;
 
+/* Commented cuz i don't need it rn
 (async () => {
-  console.log("\nProfiles:\n");
-  const profiles = await profilesClient.profile.findMany();
-  console.log(profiles);
-
-  console.log("\nServers:\n");
-  const servers = await serversClient.server.findMany();
-  console.log(servers);
-})()
+  const rapael = await rapaelClient.profile.findMany();
+  console.log("rapael connected");
+})();
+*/
 
 /* discord */
-const zcord = require('@zoe01/zcord');
-const {Client} = require('discord.js');
+const {scopePackages, zClient} = require('@zoe01/zcord');
+
+packages = scopePackages(path.join(__dirname, `../${process.env.PACKAGEPATH}`));
+
+const client = new zClient(packages);
+client.initEvents();
+client.login(process.env.TOKEN);
